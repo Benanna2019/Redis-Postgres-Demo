e7bad5a2107afd51d307be4cad918fc9ee391b62
Benjamin Patton <benjaminpatton@Benjamins-MacBook-Pro.local>
2022-02-10 10:58:08 -0500

    added packages to js files to read env info

--- psql.js
+++ psql.js
@@ -1,4 +1,5 @@
 const { Client } = require("pg");
+require("dotenv").config();
 
 let client;
 
@@ -6,7 +7,7 @@ if (process.env.NODE_ENV !== "production") {
   client = new Client();
 }
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production" && process.env.DATABASE_URL) {
   client = new Client({
     connectionString: process.env.DATABASE_URL,
   });
