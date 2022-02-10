8ff71a275ddf52f1911139ba44b8ba639d0bb73b
Benjamin Patton <benjaminpatton@Benjamins-MacBook-Pro.local>
2022-02-09 20:45:59 -0500

    added seed script to start script

--- seed.js
+++ seed.js
@@ -1,41 +1,36 @@
-const fs = require('fs')
-const psqlClient = require('./psql.js');
-require("dotenv").config()
+const fs = require("fs");
+const psqlClient = require("./psql.js");
+require("dotenv").config();
 
-// const postToDatabase = async () => {
+const postToDatabase = async () => {
+  if (process.env.NODE_ENV !== "production") {
+    const seedQuery = fs.readFileSync("./seed.data.sql", { encoding: "utf8" });
+    psqlClient.connect();
+    const result = await psqlClient.query(seedQuery, (err, res) => {
+      // console.log(err, res)
+      console.log("Seeding Completed!");
+      psqlClient.end();
+    });
+  }
+};
 
-//         if (process.env.NODE_ENV !== 'production') {
-//             const seedQuery = fs.readFileSync('./seed.data.sql', { encoding: 'utf8' })
-//             psqlClient.connect();
-//             const result = await psqlClient.query(seedQuery, (err, res) => {
-//                 // console.log(err, res)
-//                 console.log('Seeding Completed!')
-//                 psqlClient.end()
-                
-//             })
-            
-            
-//         }
+postToDatabase();
 
-// }
+// const queryDatabase = async () => {
+//   psqlClient.connect();
+//   const sql = "SELECT name from countries order by name";
+//   const result = await psqlClient.query(sql);
+//   psqlClient.end();
+//   return console.log(result.rows.map((row) => row["name"]));
+// };
 
-// postToDatabase() 
-
-const queryDatabase = async () => {
-  psqlClient.connect();
-  const sql = "SELECT name from countries order by name";
-  const result = await psqlClient.query(sql);
-  psqlClient.end()    
-  return console.log(result.rows.map(row => row['name']));
-}
-
-queryDatabase()
+// queryDatabase();
 
 // const deleteFromDatabase = async () => {
 //   psqlClient.connect();
 //   const sql = "DROP TABLE IF EXISTS countries;"
 //   const result = await psqlClient.query(sql);
-//   psqlClient.end()  
+//   psqlClient.end()
 //   console.log(result)
 // }
 
